1ef075c93b2bad85f580cece4b315180267ca594
Petri Bezemer <petri.bezemer@druid.fi>
2018-12-01 12:30:22 +0200

    Remove import

--- public/custom-service-worker.js
+++ public/custom-service-worker.js
@@ -1,5 +1,3 @@
-importScripts('/service-worker.js');
-
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
