7438d4055d6e8e0fadbc7c74d62de2925757caca
Marko Korhonen <marko.korhonen@druid.fi>
2019-01-09 22:32:54 +0200

    Try proxy as Netlify function

---
+++ functions/proxy.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (expressRequest, expressResponse) => {
+    expressResponse.send('Ping!');
+});
+
+app.post('/', (expressRequest, expressResponse) => {
+    request(expressRequest.body.url, (error, response, body) => {
+        expressResponse.type('application/xml');
+        expressResponse.send(body);
+    });
+});
+
+module.exports = app;
